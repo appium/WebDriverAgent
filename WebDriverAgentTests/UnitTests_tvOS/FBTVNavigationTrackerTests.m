ec22ab0db262f09cd84a1411f91c0a052a2db14e
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2019-05-16 00:02:15 +0900

    tweak license

--- WebDriverAgentTests/UnitTests_tvOS/FBTVNavigationTrackerTests.m
+++ WebDriverAgentTests/UnitTests_tvOS/FBTVNavigationTrackerTests.m
@@ -1,10 +1,11 @@
-//
-//  FBTVNavigationTests.m
-//  UnitTests_tvOS
-//
-//  Created by Kazuaki Matsuo on 2019/05/15.
-//  Copyright © 2019 Facebook. All rights reserved.
-//
+/**
+ * Copyright (c) 2018-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
 
 #import <XCTest/XCTest.h>
 
