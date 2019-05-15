9ec36f6fae4aab522d57c5abeb09322d3a12c5b2
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2019-05-15 22:55:10 +0900

    add unit test for tvOS

---
+++ WebDriverAgentTests/UnitTests_tvOS/FBTVNavigationTests.m
@@ -0,0 +1,9 @@
+//
+//  FBTVNavigationTests.m
+//  UnitTests_tvOS
+//
+//  Created by Kazuaki Matsuo on 2019/05/15.
+//  Copyright © 2019 Facebook. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
