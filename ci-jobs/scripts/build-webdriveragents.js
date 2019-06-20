2db5b8e2caf2e13345a1c59802b8188dcb2cf1d4
dpgraham <dpgraham.85@gmail.com>
2019-06-20 11:16:25 -0700

    CI

--- ci-jobs/scripts/build-webdriveragents.js
+++ ci-jobs/scripts/build-webdriveragents.js
@@ -10,6 +10,9 @@ async function buildAndUploadWebDriverAgents () {
   const xcodePaths = (await fs.readdir('/Applications/'))
     .filter((file) => file.toLowerCase().startsWith('xcode_'));
 
+  // Determine which xcodes need to be skipped
+  let excludedXcodeArr = (process.env.EXCLUDE_XCODE || '').split(',');
+
   for (let xcodePath of xcodePaths) {
     if (xcodePath.includes('_beta')) {
       continue;
@@ -18,6 +21,11 @@ async function buildAndUploadWebDriverAgents () {
     log.info(`Running xcode-select for '${xcodePath}'`);
     await exec('sudo', ['xcode-select', '-s', `/Applications/${xcodePath}/Contents/Developer`]);
     const xcodeVersion = xcodePath.replace('.app', '').split('_', 2)[1];
+    
+    if (excludedXcodeArr.includes(xcodeVersion)) {
+      continue;
+    }
+
     log.info('Building webdriveragent for xcode version', xcodeVersion);
     await buildWebDriverAgent(xcodeVersion);
   }
