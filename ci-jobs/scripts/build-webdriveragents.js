2dea8d1201923edaf907da870427359949556d76
dpgraham <dpgraham.85@gmail.com>
2019-06-20 10:10:12 -0700

    CI

--- ci-jobs/scripts/build-webdriveragents.js
+++ ci-jobs/scripts/build-webdriveragents.js
@@ -11,7 +11,7 @@ async function buildAndUploadWebDriverAgents () {
     .filter((file) => file.toLowerCase().startsWith('xcode_'));
 
   for (let xcodePath of xcodePaths) {
-    if (xcodePath.endsWith('_beta')) {
+    if (xcodePath.contains('_beta')) {
       continue;
     }
     // Build webdriveragent for this xcode version
