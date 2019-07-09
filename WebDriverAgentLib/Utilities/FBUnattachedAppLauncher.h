4ef51bdf6834753913d9ef3c962a97ae6b49d769
Jonathan Beyrak-Lev <jonathan@testim.io>
2019-07-09 16:35:34 +0300

    Fix copyright header on FBUnattachedAppLauncher

--- WebDriverAgentLib/Utilities/FBUnattachedAppLauncher.h
+++ WebDriverAgentLib/Utilities/FBUnattachedAppLauncher.h
@@ -1,10 +1,11 @@
-//
-//  FBUnattachedAppLauncher.h
-//  WebDriverAgentLib
-//
-//  Created by Jonathan Beyrak-Lev on 09/07/2019.
-//  Copyright © 2019 Facebook. All rights reserved.
-//
+/**
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
 
 #import <Foundation/Foundation.h>
 
