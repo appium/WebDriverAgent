a39830c90c9a464717f651fa79953f8fdc6db896
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2019-08-23 09:25:44 +0900

    replace header, assertion method

--- WebDriverAgentTests/IntegrationTests/FBConfigurationTests.m
+++ WebDriverAgentTests/IntegrationTests/FBConfigurationTests.m
@@ -1,10 +1,11 @@
-//
-//  FBConfigurationTestCase.m
-//  IntegrationTests_2
-//
-//  Created by Kazuaki Matsuo on 2019/08/22.
-//  Copyright © 2019 Facebook. All rights reserved.
-//
+/**
+* Copyright (c) 2015-present, Facebook, Inc.
+* All rights reserved.
+*
+* This source code is licensed under the BSD-style license found in the
+* LICENSE file in the root directory of this source tree. An additional grant
+* of patent rights can be found in the PATENTS file in the same directory.
+*/
 
 #import <XCTest/XCTest.h>
 #import "FBIntegrationTestCase.h"
@@ -30,14 +31,14 @@
 
   [FBConfiguration setReduceMotionEnabled:YES];
   if (isSDKVersionLessThan(@"10.0")) {
-    XCTAssertEqual([FBConfiguration reduceMotionEnabled], NO);
+    XCTAssertFalse([FBConfiguration reduceMotionEnabled]);
   } else {
-    XCTAssertEqual([FBConfiguration reduceMotionEnabled], YES);
+    XCTAssertTrue([FBConfiguration reduceMotionEnabled]);
   }
 
   [FBConfiguration setReduceMotionEnabled:defaultReduceMotionEnabled];
   if (isSDKVersionLessThan(@"10.0")) {
-    XCTAssertEqual([FBConfiguration reduceMotionEnabled], NO);
+    XCTAssertFalse([FBConfiguration reduceMotionEnabled]);
   } else {
     XCTAssertEqual([FBConfiguration reduceMotionEnabled], defaultReduceMotionEnabled);
   }
