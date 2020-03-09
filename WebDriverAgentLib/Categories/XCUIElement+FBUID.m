a922d68c1b5054250050e7f922f869e3814c34f2
Dan Maor <danm@perfectomobile.com>
2020-03-09 08:54:05 +0000

    improve findElement performance in Xcode 11

--- WebDriverAgentLib/Categories/XCUIElement+FBUID.m
+++ WebDriverAgentLib/Categories/XCUIElement+FBUID.m
@@ -11,6 +11,7 @@
 
 #import "XCUIElement+FBUtilities.h"
 #import "FBElementUtils.h"
+#import "FBXCodeCompatibility.h"
 
 @implementation XCUIElement (FBUID)
 
@@ -19,7 +20,11 @@
   if ([self respondsToSelector:@selector(accessibilityElement)]) {
     return [FBElementUtils uidWithAccessibilityElement:[self performSelector:@selector(accessibilityElement)]];
   }
-  return self.fb_lastSnapshot.fb_uid;
+  if (isSDKVersionLessThan(@"13.0")) {
+    return self.fb_lastSnapshot.fb_uid;
+  }
+  [self fb_nativeResolve];
+  return self.lastSnapshot.fb_uid;
 }
 
 @end
