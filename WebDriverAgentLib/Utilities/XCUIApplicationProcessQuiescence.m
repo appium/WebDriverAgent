12c5ec3fbe23c132ddbc5436c13e94fe4c662020
Mykola Mokhnach <mokhnach@gmail.com>
2020-10-15 22:56:03 +0200

    Update idle validation

--- WebDriverAgentLib/Utilities/XCUIApplicationProcessQuiescence.m
+++ WebDriverAgentLib/Utilities/XCUIApplicationProcessQuiescence.m
@@ -15,45 +15,49 @@
 #import "FBLogger.h"
 #import "XCUIApplicationProcess.h"
 
-static void (*original_waitForQuiescenceIncludingAnimationsIdle)(id, SEL, BOOL);
-
 
 static void swizzledWaitForQuiescenceIncludingAnimationsIdle(id self, SEL _cmd, BOOL includeAnimations)
 {
   if (!FBConfiguration.shouldWaitForQuiescence || FBConfiguration.waitForIdleTimeout < DBL_EPSILON) {
     return;
   }
-  original_waitForQuiescenceIncludingAnimationsIdle(self, _cmd, includeAnimations);
 
-  dispatch_group_t group = dispatch_group_create();
-  dispatch_queue_t queue = dispatch_queue_create("com.facebook.wda.animations", 0);
-
-  void (^activateProperty)(NSString *) = ^(NSString *propertyName) {
+  void (^setProperty)(NSString *, BOOL) = ^(NSString *propertyName, BOOL value) {
     SEL selector = NSSelectorFromString(propertyName);
     NSMethodSignature *signature = [self methodSignatureForSelector:selector];
     NSInvocation *invocation = [NSInvocation invocationWithMethodSignature:signature];
     [invocation setSelector:selector];
-    BOOL yes = YES;
-    [invocation setArgument:&yes atIndex:2];
+    [invocation setArgument:&value atIndex:2];
     [invocation invokeWithTarget:self];
   };
-  dispatch_group_async(group, queue, ^{
-    [self _notifyWhenMainRunLoopIsIdle:^{
-      activateProperty(@"setEventLoopHasIdled:");
-    }];
-  });
-  if ([self _supportsAnimationsIdleNotifications]) {
-    dispatch_group_async(group, queue, ^{
-      [self _notifyWhenAnimationsAreIdle:^{
-        activateProperty(@"setAnimationsHaveFinished:");
-      }];
-    });
+  BOOL isAnimationsIdleNotificationsSupported = [self _supportsAnimationsIdleNotifications];
+
+  setProperty(@"setEventLoopHasIdled:", NO);
+  if (isAnimationsIdleNotificationsSupported) {
+    setProperty(@"setAnimationsHaveFinished:", NO);
   }
 
+  dispatch_group_t group = dispatch_group_create();
+  dispatch_group_enter(group);
+  [self _notifyWhenMainRunLoopIsIdle:^{
+    setProperty(@"setEventLoopHasIdled:", YES);
+    dispatch_group_leave(group);
+  }];
+  if (isAnimationsIdleNotificationsSupported) {
+    dispatch_group_enter(group);
+    [self _notifyWhenAnimationsAreIdle:^{
+      setProperty(@"setAnimationsHaveFinished:", YES);
+      dispatch_group_leave(group);
+    }];
+  }
   dispatch_time_t absoluteTimeout = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(FBConfiguration.waitForIdleTimeout * NSEC_PER_SEC));
   BOOL result = 0 == dispatch_group_wait(group, absoluteTimeout);
   if (!result) {
-    [FBLogger logFmt:@"The applicaion %@ is still waiting for quiescence after %.2f seconds timeout. This timeout could be customized by 'waitForIdleTimeout' setting", [self bundleID], FBConfiguration.waitForIdleTimeout];
+    [FBLogger logFmt:@"The application %@ is still waiting for quiescence after %.2f seconds timeout. This timeout value could be customized by changing the 'waitForIdleTimeout' setting", [self bundleID], FBConfiguration.waitForIdleTimeout];
+    setProperty(@"setEventLoopHasIdled:", YES);
+    if (isAnimationsIdleNotificationsSupported) {
+      setProperty(@"setAnimationsHaveFinished:", YES);
+    }
   }
 }
 
@@ -65,7 +69,7 @@ static void swizzledWaitForQuiescenceIncludingAnimationsIdle(id self, SEL _cmd, 
   Method waitForQuiescenceMethod = class_getInstanceMethod(XCUIApplicationProcess.class, @selector(waitForQuiescenceIncludingAnimationsIdle:));
   if (waitForQuiescenceMethod != nil) {
     IMP swizzledImp = (IMP)swizzledWaitForQuiescenceIncludingAnimationsIdle;
-    original_waitForQuiescenceIncludingAnimationsIdle = (void (*)(id, SEL, BOOL))method_setImplementation(waitForQuiescenceMethod, swizzledImp);
+    method_setImplementation(waitForQuiescenceMethod, swizzledImp);
   } else {
     [FBLogger log:@"Could not find method -[XCUIApplicationProcess waitForQuiescenceIncludingAnimationsIdle:]"];
   }
