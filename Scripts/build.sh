734e4b47a69b26ea49c0727c50c8e48d1b40dd3a
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2022-02-06 19:37:29 -0800

    let me remove bundle exec

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -87,7 +87,7 @@ function xcbuild() {
 
 function fastlane_test() {
   if [[ -n "$XC_DESTINATION" ]]; then
-    SDK="$XC_SDK" DEST="$XC_DESTINATION" SCHEME="$1" bundle exec fastlane test
+    SDK="$XC_SDK" DEST="$XC_DESTINATION" SCHEME="$1" fastlane test
   else
     SDK="$XC_SDK" SCHEME="$1" bundle exec fastlane test
   fi
