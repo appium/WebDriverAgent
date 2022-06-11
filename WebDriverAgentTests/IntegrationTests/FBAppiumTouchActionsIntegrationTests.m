2a3e17ca92766e3f2b470a8c7585ba5c3ebc5866
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2022-06-10 22:44:45 -0700

    Revert "remove force touch test code stuff"
    This reverts commit 6452ac4045dd731d86d81506559ace4204d8a4ad.

--- WebDriverAgentTests/IntegrationTests/FBAppiumTouchActionsIntegrationTests.m
+++ WebDriverAgentTests/IntegrationTests/FBAppiumTouchActionsIntegrationTests.m
@@ -285,6 +285,36 @@
   [self verifyGesture:gesture orientation:orientation];
 }
 
+// Failed to press in iOS 16
+- (void)testForcePress
+{
+  if (SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(@"15.0")) {
+    // Does not work on iOS 15.
+    return;
+  }
+  NSArray<NSDictionary<NSString *, id> *> *gesture =
+  @[@{
+      @"action": @"press",
+      @"options": @{
+          @"ELEMENT": self.testedApplication.buttons[FBShowAlertForceTouchButtonName],
+          @"x": @1,
+          @"y": @1,
+          @"pressure": @1
+          }
+      },
+    @{
+      @"action": @"wait",
+      @"options": @{
+          @"ms": @300
+          }
+      },
+    @{
+      @"action": @"release"
+      }
+    ];
+  [self verifyGesture:gesture orientation:UIDeviceOrientationPortrait];
+}
+
 @end
 
 
