b21e287cad447dbab4f90a4c720a7d4855e69510
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2022-12-23 10:17:39 -0800

    chore: use UPPER_CASE for const

--- Scripts/build-webdriveragent.js
+++ Scripts/build-webdriveragent.js
@@ -4,43 +4,43 @@ const { logger, fs, zip } = require('@appium/support');
 const { exec } = require('teen_process');
 const xcode = require('appium-xcode');
 
-const log = new logger.getLogger('WDABuild');
-const rootDir = path.resolve(__dirname, '..');
-const derivedDataPath = `${rootDir}/wdaBuild`;
-const wdaAppBundle = 'WebDriverAgentRunner-Runner.app';
-const appBundlePath = path.join(derivedDataPath, 'Build', 'Products', 'Debug-iphonesimulator');
+const LOG = new logger.getLogger('WDABuild');
+const ROOT_DIR = path.resolve(__dirname, '..');
+const DERIVED_DATA_PATH = `${ROOT_DIR}/wdaBuild`;
+const WDA_BUNDLE = 'WebDriverAgentRunner-Runner.app';
+const WDA_BUNDLE_PATH = path.join(DERIVED_DATA_PATH, 'Build', 'Products', 'Debug-iphonesimulator');
 
 async function buildWebDriverAgent (xcodeVersion) {
-  log.info(`Deleting ${derivedDataPath} if exists`);
-  if (await fs.exists(derivedDataPath)) {
-    await fs.rimraf(derivedDataPath);
+  LOG.info(`Deleting ${DERIVED_DATA_PATH} if exists`);
+  if (await fs.exists(DERIVED_DATA_PATH)) {
+    await fs.rimraf(DERIVED_DATA_PATH);
   }
 
   // Get Xcode version
   xcodeVersion = xcodeVersion || await xcode.getVersion();
-  log.info(`Building WebDriverAgent for iOS for Xcode version '${xcodeVersion}'`);
+  LOG.info(`Building WebDriverAgent for iOS for Xcode version '${xcodeVersion}'`);
 
   // Clean and build
   try {
     await exec('/bin/bash', ['./Scripts/build.sh'], {
-      env: {TARGET: 'runner', SDK: 'sim', DERIVED_DATA_PATH: derivedDataPath},
-      cwd: rootDir
+      env: {TARGET: 'runner', SDK: 'sim', DERIVED_DATA_PATH},
+      cwd: ROOT_DIR
     });
   } catch (e) {
-    log.error(`===FAILED TO BUILD FOR ${xcodeVersion}`);
-    log.error(e.stdout);
-    log.error(e.stderr);
-    log.error(e.message);
+    LOG.error(`===FAILED TO BUILD FOR ${xcodeVersion}`);
+    LOG.error(e.stdout);
+    LOG.error(e.stderr);
+    LOG.error(e.message);
     throw e;
   }
 
   const zipName = `WebDriverAgentRunner-Runner-Sim-${xcodeVersion}.zip`;
-  log.info(`Creating ${zipName} which includes ${wdaAppBundle}`);
-  const appBundleZipPath = path.join(rootDir, zipName);
+  LOG.info(`Creating ${zipName} which includes ${WDA_BUNDLE}`);
+  const appBundleZipPath = path.join(ROOT_DIR, zipName);
   await fs.rimraf(appBundleZipPath);
-  log.info(`Created './${zipName}'`);
-  await zip.toArchive(appBundleZipPath, {pattern: '*.app/**', cwd: appBundlePath});
-  log.info(`Zip bundled at "${appBundleZipPath}"`);
+  LOG.info(`Created './${zipName}'`);
+  await zip.toArchive(appBundleZipPath, {pattern: '*.app/**', cwd: WDA_BUNDLE_PATH});
+  LOG.info(`Zip bundled at "${appBundleZipPath}"`);
 }
 
 if (require.main === module) {
