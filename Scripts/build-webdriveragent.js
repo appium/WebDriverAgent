5a1ba782897049395825741f571d11cf787b6a69
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2023-01-27 23:50:57 -0800

    chore: build:tv for tvOS

--- Scripts/build-webdriveragent.js
+++ Scripts/build-webdriveragent.js
@@ -10,6 +10,9 @@ const DERIVED_DATA_PATH = `${ROOT_DIR}/wdaBuild`;
 const WDA_BUNDLE = 'WebDriverAgentRunner-Runner.app';
 const WDA_BUNDLE_PATH = path.join(DERIVED_DATA_PATH, 'Build', 'Products', 'Debug-iphonesimulator');
 
+const WDA_BUNDLE_TV = 'WebDriverAgentRunner_tvOS-Runner.app';
+const WDA_BUNDLE_TV_PATH = path.join(DERIVED_DATA_PATH, 'Build', 'Products', 'Debug-appletvsimulator');
+
 async function buildWebDriverAgent (xcodeVersion) {
   LOG.info(`Cleaning ${DERIVED_DATA_PATH} if exists`);
   try {
@@ -25,7 +28,7 @@ async function buildWebDriverAgent (xcodeVersion) {
   // Clean and build
   try {
     await exec('/bin/bash', ['./Scripts/build.sh'], {
-      env: {TARGET: 'runner', SDK: 'sim', DERIVED_DATA_PATH},
+      env: {TARGET: process.env.TARGET, SDK: process.env.SDK, DERIVED_DATA_PATH},
       cwd: ROOT_DIR
     });
   } catch (e) {
@@ -34,14 +37,18 @@ async function buildWebDriverAgent (xcodeVersion) {
     throw e;
   }
 
-  const zipName = `WebDriverAgentRunner-Runner-Sim-${xcodeVersion}.zip`;
-  LOG.info(`Creating ${zipName} which includes ${WDA_BUNDLE}`);
+  const isTv = process.env.TARGET === 'tv_runner';
+  const bundle = isTv ? WDA_BUNDLE_TV : WDA_BUNDLE;
+  const bundle_path = isTv ? WDA_BUNDLE_TV_PATH : WDA_BUNDLE_PATH;
+
+  const zipName = `WebDriverAgentRunner-Runner-${process.env.SDK}-${xcodeVersion}.zip`;
+  LOG.info(`Creating ${zipName} which includes ${bundle}`);
   const appBundleZipPath = path.join(ROOT_DIR, zipName);
   await fs.rimraf(appBundleZipPath);
   LOG.info(`Created './${zipName}'`);
   try {
-    await exec('xattr', ['-cr', WDA_BUNDLE], {cwd: WDA_BUNDLE_PATH});
-    await exec('zip', ['-qr', appBundleZipPath, WDA_BUNDLE], {cwd: WDA_BUNDLE_PATH});
+    await exec('xattr', ['-cr', bundle], {cwd: bundle_path});
+    await exec('zip', ['-qr', appBundleZipPath, bundle], {cwd: bundle_path});
   } catch (e) {
     LOG.error(`===FAILED TO ZIP ARCHIVE`);
     LOG.error(e.stderr);
