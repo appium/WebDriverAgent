03557d791b231aae339d1add3ff18f373ed34539
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2023-05-29 22:08:27 -0700

    ci: tweak paths

--- Scripts/build-ci.sh
+++
@@ -1,8 +0,0 @@
-# To run build script for CI
-
-xcodebuild clean build-for-testing \
-  -project WebDriverAgent.xcodeproj \
-  -derivedDataPath $PKG_PATH_IOS_SIM_X86_64 \
-  -scheme "$SCHEME" \
-  -destination "$DESTINATION" \
-  CODE_SIGNING_ALLOWED=NO ARCHS="$ARCHS"
---
+++ Scripts/ci/build-ci.sh
@@ -0,0 +1,8 @@
+# To run build script for CI
+
+xcodebuild clean build-for-testing \
+  -project WebDriverAgent.xcodeproj \
+  -derivedDataPath $DERIVED_DATA_PATH \
+  -scheme $SCHEME \
+  -destination $DESTINATION \
+  CODE_SIGNING_ALLOWED=NO ARCHS=$ARCHS
