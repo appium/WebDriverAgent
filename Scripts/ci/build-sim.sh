9fddbb25895958c3537fee563e89d8b8e9231f7d
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2025-06-22 10:58:18 -0700

    ci: remove unnecessary files from the prebuilt package

--- Scripts/ci/build-sim.sh
+++ Scripts/ci/build-sim.sh
@@ -14,6 +14,14 @@ xcodebuild clean build-for-testing \
 pushd wda_build
 # to remove unnecessary space consuming files
 rm -rf Build/Intermediates.noindex
+
+# Xcode 16 started generating 5.9MB of 'Testing.framework', but it might not be necessary for WDA
+rm -rf Build/**/$SCHEME-Runner.app/Frameworks/Testing.framework
+
+# This library is used for Swift testing. WDA doesn't include Swift stuff, thus this is not needed.
+# Xcode 16 generates a 2.6 MB file size. Xcode 15 was a 1 MB file size.
+rm -rf Build/**/$SCHEME-Runner.app/Frameworks/libXCTestSwiftSupport.dylib
+
 zip -r $ZIP_PKG_NAME Build
 popd
 mv wda_build/$ZIP_PKG_NAME ./
