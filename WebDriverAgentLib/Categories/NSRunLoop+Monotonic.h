c9bf35ad61f67a27e60e6c72249530e5422d9457
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2025-08-16 16:22:56 +0900

    add docstring

--- WebDriverAgentLib/Categories/NSRunLoop+Monotonic.h
+++ WebDriverAgentLib/Categories/NSRunLoop+Monotonic.h
@@ -1,5 +1,14 @@
 #import <Foundation/Foundation.h>
 
 @interface NSRunLoop (Monotonic)
+/**
+ Runs the current run loop in the default mode for the specified monotonic time interval.
+
+ This method uses a monotonic clock to ensure the interval is not affected
+ by changes to the system clock. The run loop will repeatedly process events
+ until the given number of seconds has elapsed.
+
+ @param seconds The duration, in seconds, to run the run loop.
+ */
 - (void)runForMonotonicInterval:(NSTimeInterval)seconds;
 @end
