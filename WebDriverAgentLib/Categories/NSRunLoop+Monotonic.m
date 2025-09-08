26c10597b28b3ea627a7dd7b1306428ae63d125c
codeskyblue <codeskyblue@gmail.com>
2025-09-08 08:12:21 +0800

    add docstring

--- WebDriverAgentLib/Categories/NSRunLoop+Monotonic.m
+++ WebDriverAgentLib/Categories/NSRunLoop+Monotonic.m
@@ -3,6 +3,12 @@
 
 @implementation NSRunLoop (Monotonic)
 
+/**
+ * Runs the current run loop for a specified monotonic interval (in seconds),
+ * using CACurrentMediaTime for time measurement. This method is not affected by system time changes.
+ *
+ * @param seconds The duration to run the run loop, measured in monotonic time.
+ */
 - (void)runForMonotonicInterval:(NSTimeInterval)seconds {
     CFTimeInterval end = CACurrentMediaTime() + seconds;
     while (CACurrentMediaTime() < end) {
