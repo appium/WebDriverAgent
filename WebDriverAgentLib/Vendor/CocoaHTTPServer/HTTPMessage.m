ce7e2453e999f4e829b8b92d10a73d64ac6bdfb8
Mykola Mokhnach <mokhnach@gmail.com>
2025-12-02 23:07:11 +0100

    feat: Deprecate CFNetwork usage

--- WebDriverAgentLib/Vendor/CocoaHTTPServer/HTTPMessage.m
+++ WebDriverAgentLib/Vendor/CocoaHTTPServer/HTTPMessage.m
@@ -8,107 +8,291 @@
 
 @implementation HTTPMessage
 
-- (id)initEmptyRequest
+- (id)init
 {
   if ((self = [super init]))
   {
-    message = CFHTTPMessageCreateEmpty(NULL, YES);
+    _headers = [[NSMutableDictionary alloc] init];
+    _body = [[NSMutableData alloc] init];
+    _rawData = [[NSMutableData alloc] init];
+    _version = HTTPVersion1_1;
+    _headerComplete = NO;
+    _isRequest = YES;
+  }
+  return self;
+}
+
+- (id)initEmptyRequest
+{
+  if ((self = [self init]))
+  {
+    _isRequest = YES;
   }
   return self;
 }
 
 - (id)initRequestWithMethod:(NSString *)method URL:(NSURL *)url version:(NSString *)version
 {
-  if ((self = [super init]))
+  if ((self = [self init]))
   {
-    message = CFHTTPMessageCreateRequest(NULL,
-                                         (__bridge CFStringRef)method,
-                                         (__bridge CFURLRef)url,
-                                         (__bridge CFStringRef)version);
+    _isRequest = YES;
+    _method = [method copy];
+    _url = [url copy];
+    _version = version ? [version copy] : HTTPVersion1_1;
   }
   return self;
 }
 
 - (id)initResponseWithStatusCode:(NSInteger)code description:(NSString *)description version:(NSString *)version
 {
-  if ((self = [super init]))
+  if ((self = [self init]))
   {
-    message = CFHTTPMessageCreateResponse(NULL,
-                                          (CFIndex)code,
-                                          (__bridge CFStringRef)description,
-                                          (__bridge CFStringRef)version);
+    _isRequest = NO;
+    _statusCode = code;
+    _statusDescription = [description copy];
+    _version = version ? [version copy] : HTTPVersion1_1;
   }
   return self;
 }
 
-- (void)dealloc
-{
-  if (message)
-  {
-    CFRelease(message);
-  }
-}
-
 - (BOOL)appendData:(NSData *)data
 {
-  return CFHTTPMessageAppendBytes(message, [data bytes], [data length]);
+  if (!data || [data length] == 0)
+  {
+    return NO;
+  }
+
+  [_rawData appendData:data];
+
+  if (!_headerComplete)
+  {
+    // Look for the end of headers (CRLF CRLF or LF LF)
+    NSData *headerEndMarker = [@"\r\n\r\n" dataUsingEncoding:NSASCIIStringEncoding];
+    NSRange headerEndRange = [_rawData rangeOfData:headerEndMarker options:0 range:NSMakeRange(0, [_rawData length])];
+
+    if (headerEndRange.location == NSNotFound)
+    {
+      // Also check for LF LF (some clients use this)
+      NSData *lfMarker = [@"\n\n" dataUsingEncoding:NSASCIIStringEncoding];
+      headerEndRange = [_rawData rangeOfData:lfMarker options:0 range:NSMakeRange(0, [_rawData length])];
+    }
+
+    if (headerEndRange.location != NSNotFound)
+    {
+      _headerComplete = YES;
+
+      // Parse the header data
+      NSData *headerData = [_rawData subdataWithRange:NSMakeRange(0, headerEndRange.location + headerEndRange.length)];
+      NSString *headerString = [[NSString alloc] initWithData:headerData encoding:NSASCIIStringEncoding];
+
+      if (headerString)
+      {
+        [self parseHeaders:headerString];
+      }
+
+      // Extract body data if any
+      NSUInteger bodyStart = headerEndRange.location + headerEndRange.length;
+      if ([_rawData length] > bodyStart)
+      {
+        NSData *bodyData = [_rawData subdataWithRange:NSMakeRange(bodyStart, [_rawData length] - bodyStart)];
+        [_body appendData:bodyData];
+      }
+
+      [_rawData setLength:0];
+    }
+  }
+  else
+  {
+    // Headers are complete, append to body
+    [_body appendData:data];
+  }
+
+  return YES;
+}
+
+- (void)parseHeaders:(NSString *)headerString
+{
+  NSArray *lines = [headerString componentsSeparatedByString:@"\r\n"];
+  if ([lines count] == 0)
+  {
+    lines = [headerString componentsSeparatedByString:@"\n"];
+  }
+
+  if ([lines count] == 0)
+  {
+    return;
+  }
+
+  // Parse first line (request line or status line)
+  NSString *firstLine = [lines objectAtIndex:0];
+  NSArray *firstLineParts = [firstLine componentsSeparatedByString:@" "];
+
+  if (_isRequest && [firstLineParts count] >= 3)
+  {
+    // Request line: METHOD URL VERSION
+    _method = [firstLineParts objectAtIndex:0];
+    NSString *urlString = [firstLineParts objectAtIndex:1];
+    _url = [NSURL URLWithString:urlString];
+    if ([firstLineParts count] >= 3)
+    {
+      _version = [firstLineParts objectAtIndex:2];
+    }
+  }
+  else if (!_isRequest && [firstLineParts count] >= 3)
+  {
+    // Status line: VERSION CODE DESCRIPTION
+    _version = [firstLineParts objectAtIndex:0];
+    _statusCode = [[firstLineParts objectAtIndex:1] integerValue];
+    NSMutableArray *descParts = [NSMutableArray arrayWithArray:firstLineParts];
+    [descParts removeObjectAtIndex:0];
+    [descParts removeObjectAtIndex:0];
+    _statusDescription = [descParts componentsJoinedByString:@" "];
+  }
+
+  // Parse header fields
+  for (NSUInteger i = 1; i < [lines count]; i++)
+  {
+    NSString *line = [lines objectAtIndex:i];
+    if ([line length] == 0)
+    {
+      continue;
+    }
+
+    NSRange colonRange = [line rangeOfString:@":"];
+    if (colonRange.location != NSNotFound)
+    {
+      NSString *headerName = [[line substringToIndex:colonRange.location] stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceCharacterSet]];
+      NSString *headerValue = [[line substringFromIndex:colonRange.location + 1] stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceCharacterSet]];
+
+      if ([headerName length] > 0)
+      {
+        // HTTP headers are case-insensitive, but we'll store them with their original case
+        // For lookup, we'll use case-insensitive comparison
+        [_headers setObject:headerValue forKey:headerName];
+      }
+    }
+  }
 }
 
 - (BOOL)isHeaderComplete
 {
-  return CFHTTPMessageIsHeaderComplete(message);
+  return _headerComplete;
 }
 
 - (NSString *)version
 {
-  return (__bridge_transfer NSString *)CFHTTPMessageCopyVersion(message);
+  return _version;
 }
 
 - (NSString *)method
 {
-  return (__bridge_transfer NSString *)CFHTTPMessageCopyRequestMethod(message);
+  return _method;
 }
 
 - (NSURL *)url
 {
-  return (__bridge_transfer NSURL *)CFHTTPMessageCopyRequestURL(message);
+  return _url;
 }
 
 - (NSInteger)statusCode
 {
-  return (NSInteger)CFHTTPMessageGetResponseStatusCode(message);
+  return _statusCode;
 }
 
 - (NSDictionary *)allHeaderFields
 {
-  return (__bridge_transfer NSDictionary *)CFHTTPMessageCopyAllHeaderFields(message);
+  return [_headers copy];
 }
 
 - (NSString *)headerField:(NSString *)headerField
 {
-  return (__bridge_transfer NSString *)CFHTTPMessageCopyHeaderFieldValue(message, (__bridge CFStringRef)headerField);
+  // Case-insensitive lookup
+  for (NSString *key in [_headers allKeys])
+  {
+    if ([key caseInsensitiveCompare:headerField] == NSOrderedSame)
+    {
+      return [_headers objectForKey:key];
+    }
+  }
+  return nil;
 }
 
 - (void)setHeaderField:(NSString *)headerField value:(NSString *)headerFieldValue
 {
-  CFHTTPMessageSetHeaderFieldValue(message,
-                                   (__bridge CFStringRef)headerField,
-                                   (__bridge CFStringRef)headerFieldValue);
+  if (headerField && headerFieldValue)
+  {
+    // Remove existing header with same name (case-insensitive)
+    NSMutableArray *keysToRemove = [NSMutableArray array];
+    for (NSString *key in [_headers allKeys])
+    {
+      if ([key caseInsensitiveCompare:headerField] == NSOrderedSame)
+      {
+        [keysToRemove addObject:key];
+      }
+    }
+    [_headers removeObjectsForKeys:keysToRemove];
+
+    // Add new header
+    [_headers setObject:headerFieldValue forKey:headerField];
+  }
 }
 
 - (NSData *)messageData
 {
-  return (__bridge_transfer NSData *)CFHTTPMessageCopySerializedMessage(message);
+  NSMutableString *messageString = [NSMutableString string];
+
+  if (_isRequest)
+  {
+    // Request line
+    NSString *urlString = [_url absoluteString];
+    if (!urlString)
+    {
+      urlString = [_url path];
+    }
+    [messageString appendFormat:@"%@ %@ %@\r\n", _method ?: @"GET", urlString ?: @"/", _version ?: HTTPVersion1_1];
+  }
+  else
+  {
+    // Status line
+    [messageString appendFormat:@"%@ %ld %@\r\n", _version ?: HTTPVersion1_1, (long)_statusCode, _statusDescription ?: @""];
+  }
+
+  // Headers
+  for (NSString *key in [_headers allKeys])
+  {
+    NSString *value = [_headers objectForKey:key];
+    [messageString appendFormat:@"%@: %@\r\n", key, value];
+  }
+
+  // Empty line to separate headers from body
+  [messageString appendString:@"\r\n"];
+
+  NSMutableData *data = [NSMutableData dataWithData:(id)[messageString dataUsingEncoding:NSASCIIStringEncoding]];
+
+  // Append body if present
+  if ([_body length] > 0)
+  {
+    [data appendData:_body];
+  }
+
+  return data;
 }
 
 - (NSData *)body
 {
-  return (__bridge_transfer NSData *)CFHTTPMessageCopyBody(message);
+  return [_body copy];
 }
 
 - (void)setBody:(NSData *)body
 {
-  CFHTTPMessageSetBody(message, (__bridge CFDataRef)body);
+  if (body)
+  {
+    _body = [body mutableCopy];
+  }
+  else
+  {
+    _body = [[NSMutableData alloc] init];
+  }
 }
 
 @end
