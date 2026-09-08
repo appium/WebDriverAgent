cef1964e2291e2b6f77b1a2ba1474d293865b478
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2026-09-07 21:01:43 -0700

    Revert "improve"
    This reverts commit 689083428b9a68ddf04ed4e88f2ed048f28f1a86.

--- WebDriverAgentTests/IntegrationTests/FBAutoAlertsHandlerTests.m
+++ WebDriverAgentTests/IntegrationTests/FBAutoAlertsHandlerTests.m
@@ -27,12 +27,6 @@
 {
   [super setUp];
 
-  // Both tests skip in CI. Do not launch the app before reaching their skip
-  // checks: a launch timeout would turn an intended skip into a failure.
-  if (FBIntegrationTestCase.isRunningInCI) {
-    return;
-  }
-
   [self launchApplication];
   [self goToAlertsPage];
 
@@ -41,9 +35,7 @@
 
 - (void)tearDown
 {
-  if (!FBIntegrationTestCase.isRunningInCI) {
-    [self clearAlert];
-  }
+  [self clearAlert];
 
   if (self.session) {
     [self.session kill];
